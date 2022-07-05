476c388d430ca41887efe07611dc81b38ff4d39e
mykolao <mykolaoleskiv@gmail.com>
2022-07-05 16:06:10 +0300

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ const server = new ApolloServer({
     cors: false,
 });
 
-server.listen().then(({ url }) => {
+server.listen({port: process.env.PORT || 4000}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
